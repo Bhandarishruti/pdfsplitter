cb405e5a36103d3220ef778a3bad24e4903da389
Shruti Bhandari <bhandarishruti2808@gmail.com>
2023-11-30 21:51:59 +0530

    Initial Commit

---
+++ src/main/java/com/internship/pdfsplitter/Pdfsplitter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.internship.pdfsplitter;
+
+/**
+ *
+ * @author DELL
+ */
+import org.apache.pdfbox.pdmodel.PDDocument;
+import java.io.File;
+import java.io.IOException;
+
+public class Pdfsplitter {
+    // ... (unchanged code)
+}
+
